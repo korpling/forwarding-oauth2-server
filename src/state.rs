bb506652439e2aec7e585df64408404f9cc29f68
Thomas Krause <thomaskrause@posteo.de>
2021-07-08 16:02:06 +0200

    Remove with_solicitor

--- src/state.rs
+++ src/state.rs
@@ -42,23 +42,6 @@ impl State {
         };
         Ok(state)
     }
-
-    pub fn with_solicitor<'a, S>(
-        &'a mut self,
-        solicitor: S,
-    ) -> impl Endpoint<OAuthRequest, Error = WebError> + 'a
-    where
-        S: OwnerSolicitor<OAuthRequest> + 'static,
-    {
-        ErrorInto::new(Generic {
-            authorizer: &mut self.endpoint.authorizer,
-            registrar: &mut self.endpoint.registrar,
-            issuer: &mut self.endpoint.issuer,
-            solicitor,
-            scopes: &mut self.endpoint.scopes,
-            response: OAuthResponse::ok,
-        })
-    }
 }
 
 impl Actor for State {
@@ -72,35 +55,8 @@ where
     type Result = Result<Op::Item, Op::Error>;
 
     fn handle(&mut self, msg: OAuthMessage<Op, Extras>, _: &mut Self::Context) -> Self::Result {
-        let (op, ex) = msg.into_inner();
+        let (op, _) = msg.into_inner();
 
-        match ex {
-            Extras::AuthGet => {
-                let solicitor =
-                    FnSolicitor(move |_: &mut OAuthRequest, pre_grant: Solicitation| {
-                        // This will display a page to the user asking for his permission to proceed. The submitted form
-                        // will then trigger the other authorization handler which actually completes the flow.
-                        OwnerConsent::InProgress(
-                            OAuthResponse::ok().content_type("text/html").unwrap().body(
-                                &"TODO",
-                            ),
-                        )
-                    });
-
-                op.run(self.with_solicitor(solicitor))
-            }
-            Extras::AuthPost(query_string) => {
-                let solicitor = FnSolicitor(move |_: &mut OAuthRequest, _: Solicitation| {
-                    if query_string.contains("allow") {
-                        OwnerConsent::Authorized("dummy user".to_owned())
-                    } else {
-                        OwnerConsent::Denied
-                    }
-                });
-
-                op.run(self.with_solicitor(solicitor))
-            }
-            _ => op.run(&mut self.endpoint),
-        }
+        op.run(&mut self.endpoint)
     }
 }
