e1067bd0862794ad35803c0ccc1995e3a19333a1
Thomas Krause <thomaskrause@posteo.de>
2021-07-08 18:30:35 +0200

    Deny or authorize by presence of an environment variable

--- src/api.rs
+++ src/api.rs
@@ -14,7 +14,12 @@ pub async fn get_authorize(
         state
             .endpoint()
             .with_solicitor(FnSolicitor(move |_: &mut _, _grant: Solicitation<'_>| {
-                OwnerConsent::Denied
+                let remote_user = std::env::var("REMOTE_USER").unwrap_or_default();
+                if remote_user.is_empty() {
+                    OwnerConsent::Denied
+                } else {
+                    OwnerConsent::Authorized(remote_user)
+                }
             }));
 
     AuthorizationFlow::prepare(endpoint)?
@@ -38,9 +43,11 @@ pub async fn refresh() -> Result<HttpResponse, ServiceError> {
 mod tests {
     use super::*;
     use actix_web::{test, web, App};
+    use serial_test::serial;
 
     #[actix_rt::test]
-    async fn test_get_authorize() {
+    #[serial]
+    async fn test_get_authorize_noenv() {
         let state = State::new().unwrap();
         let mut app = test::init_service(
             App::new()
@@ -48,6 +55,8 @@ mod tests {
                 .route("/authorize", web::get().to(get_authorize)),
         )
         .await;
+
+        std::env::set_var("REMOTE_USER", "");
 
         let req = test::TestRequest::with_uri("/authorize?response_type=code&client_id=ANNIS&redirect_uri=http%3A%2F%2Flocalhost%3A5712&scope=default-scope&state=23235253").to_request();
         let resp = test::call_service(&mut app, req).await;
@@ -59,5 +68,27 @@ mod tests {
             location,
             "http://localhost:5712/?state=23235253&error=access_denied"
         );
+    }
+
+    #[actix_rt::test]
+    #[serial]
+    async fn test_get_authorize_remove_user_present() {
+        let state = State::new().unwrap();
+        let mut app = test::init_service(
+            App::new()
+                .data(state)
+                .route("/authorize", web::get().to(get_authorize)),
+        )
+        .await;
+
+        std::env::set_var("REMOTE_USER", "testuser@example.com");
+
+        let req = test::TestRequest::with_uri("/authorize?response_type=code&client_id=ANNIS&redirect_uri=http%3A%2F%2Flocalhost%3A5712&scope=default-scope&state=23235253").to_request();
+        let resp = test::call_service(&mut app, req).await;
+
+        assert_eq!(resp.status(), 302);
+        assert!(resp.headers().get("location").is_some());
+        let location = resp.headers().get("location").unwrap().to_str().unwrap();
+        assert!(location.contains("code="));
     }
 }
