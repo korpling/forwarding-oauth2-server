5af7a1f82f44b336d967ab24a2a2058fca459a2e
Thomas Krause <thomaskrause@posteo.de>
2021-07-15 11:51:24 +0200

    Use the HeaderExtensions when creating authorization tokens

--- src/api.rs
+++ src/api.rs
@@ -3,8 +3,8 @@ use std::collections::HashMap;
 use actix_web::{web, HttpRequest};
 use oxide_auth::{
     endpoint::{
-        AccessTokenExtension, AccessTokenFlow, AuthorizationFlow, Extension,
-        OwnerConsent, RefreshFlow, Solicitation,
+        AccessTokenExtension, AccessTokenFlow, AuthorizationExtension, AuthorizationFlow,
+        Extension, OwnerConsent, RefreshFlow, Solicitation,
     },
     frontends::simple::{endpoint::FnSolicitor, extensions::Extended},
     primitives::grant::{Extensions, Value},
@@ -20,6 +20,19 @@ struct HeaderExtension {
 impl Extension for HeaderExtension {
     fn access_token(&mut self) -> Option<&mut dyn AccessTokenExtension> {
         Some(self)
+    }
+}
+
+impl AuthorizationExtension for HeaderExtension {
+    fn extend(
+        &mut self,
+        _request: &dyn oxide_auth::code_grant::authorization::Request,
+    ) -> std::result::Result<Extensions, ()> {
+        let mut extensions = Extensions::new();
+        for (n, v) in &self.headers {
+            extensions.set_raw(n.to_string(), Value::Public(Some(v.to_string())));
+        }
+        Ok(extensions)
     }
 }
 
@@ -54,34 +67,33 @@ pub async fn authorize(
         },
     ));
 
-    // Add all filtered headers to map
-    // TODO: allow to configure the filter criterion
-    let extension = HeaderExtension {
-        headers: http_req
-            .headers()
-            .iter()
-            .filter(|(name, _)| name.as_str().starts_with("X-"))
-            .map(|(name, value)| {
-                (
-                    name.to_string(),
-                    value.to_str().unwrap_or_default().to_string(),
-                )
-            })
-            .collect(),
-    };
-
-    let extended = Extended::extend_with(endpoint, extension);
-
-    AuthorizationFlow::prepare(extended)?
+    AuthorizationFlow::prepare(endpoint)?
         .execute(auth_request)
         .map_err(WebError::from)
 }
 
 pub async fn token(
-    (auth_request, state): (OAuthRequest, web::Data<State>),
+    (auth_request, http_req, state): (OAuthRequest, HttpRequest, web::Data<State>),
 ) -> Result<OAuthResponse, WebError> {
     let endpoint = state.endpoint();
-    AccessTokenFlow::prepare(endpoint)?
+    // Add all filtered headers to map
+    // TODO: allow to configure the filter criterion
+    let headers: HashMap<_, _> = http_req
+        .headers()
+        .iter()
+        .filter(|(name, _)| name.as_str().starts_with("x-"))
+        .map(|(name, value)| {
+            (
+                name.to_string(),
+                value.to_str().unwrap_or_default().to_string(),
+            )
+        })
+        .collect();
+    let extension = HeaderExtension { headers };
+
+    let extended = Extended::extend_with(endpoint, extension);
+
+    AccessTokenFlow::prepare(extended)?
         .execute(auth_request)
         .map_err(WebError::from)
 }
